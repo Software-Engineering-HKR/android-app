87e77ef4cf6febdda33559388106d0f54657e051
smallpotato <smallpotato6035@gmail.com>
2024-03-23 22:34:16 +0100

    move compostables into separate file, handle dismissing keyboard

--- app/src/main/java/se/hkr/smarthouse/MainActivity.kt
+++ app/src/main/java/se/hkr/smarthouse/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -29,7 +30,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
@@ -38,7 +41,9 @@ import androidx.compose.ui.unit.sp
 import se.hkr.smarthouse.data.Device
 import se.hkr.smarthouse.data.Sensor
 import se.hkr.smarthouse.network.WSHelper
+import se.hkr.smarthouse.ui.composables.DeviceCard
 import se.hkr.smarthouse.ui.composables.DevicesComposables
+import se.hkr.smarthouse.ui.composables.SensorCard
 import se.hkr.smarthouse.ui.theme.SmartHouseTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,6 +52,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             val scrollState = rememberScrollState()
+            val focusManager = LocalFocusManager.current
 
             WSHelper.devices.apply {
                 add(Device(name = "led", endpoint = "led", displayName = "White Light", status = remember { mutableStateOf(false) }))
@@ -70,7 +76,9 @@ class MainActivity : ComponentActivity() {
             SmartHouseTheme {
                         // A surface container using the 'background' color from the theme
                         Surface(
-                            modifier = Modifier.fillMaxSize(),
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .pointerInput(Unit) { detectTapGestures(onTap = { focusManager.clearFocus() })},
                             color = MaterialTheme.colorScheme.background
                         ) {
                             //ScaffoldExample()
@@ -82,12 +90,14 @@ class MainActivity : ComponentActivity() {
                                     .fillMaxSize()
                                     .padding(horizontal = 16.dp, vertical = 16.dp)
                             ) {
-                                Text(
+                                /*Text(
                                     "Welcome, user!",
                                     style = MaterialTheme.typography.headlineMedium,
                                     color = Color.Black,
-                                    modifier = Modifier.align(Alignment.Start)
-                                )
+                                    modifier = Modifier
+                                        .align(Alignment.Start)
+                                        .padding(horizontal = 16.dp)
+                                )*/
 
                                 val Composables = DevicesComposables()
                                 Composables.TextInputCard(LCDText)
@@ -110,109 +120,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun DeviceSwitch(device: Device) {
-    Switch(
-        modifier = Modifier.semantics { contentDescription = "Demo" },
-        checked = device.status.value,
-        onCheckedChange = { isChecked ->
-            WSHelper.toggleDevice(device.status, device.endpoint)
-            device.status.value = isChecked
-        })
-}
-
-@Composable
-fun DeviceCard(device: Device) {
-    ElevatedCard(
-        Modifier
-            .fillMaxWidth()
-            .padding(0.4.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.secondaryContainer,
-            contentColor = MaterialTheme.colorScheme.onSecondaryContainer
-        )
-    ) {
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .padding(4.dp)
-                .fillMaxWidth()
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.Start,
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp, vertical = 8.dp)
-                ) {
-                    Icon(imageVector = Icons.Rounded.House, contentDescription = null)
-                }
-
-                Column(
-                    horizontalAlignment = Alignment.Start,
-                    modifier = Modifier
-                        .padding(horizontal = 8.dp, vertical = 8.dp)
-                ) {
-                    Text(
-                        text = device.displayName.uppercase(),
-                        style = MaterialTheme.typography.headlineMedium,
-                        modifier = Modifier.align(Alignment.Start),
-                    )
-                    Text(
-                        text = (if (device.status.value) device.statusMaskTrue else device.statusMaskFalse).uppercase(),
-                        style = MaterialTheme.typography.bodySmall,
-                        modifier = Modifier.align(Alignment.Start),
-                    )
-                }
-            }
-
-            Column(
-                horizontalAlignment = Alignment.End,
-                //modifier = Modifier
-                    //.padding(16.dp),
-            ) {
-                DeviceSwitch(device)
-            }
-        }
-    }
-}
-
-@Composable
-fun SensorCard(
-    text: String,
-    navigateTo: Class<*>,
-    modifier: Modifier = Modifier
-) {
-    val context = LocalContext.current
-    ElevatedCard(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(horizontal = 0.dp, vertical = 8.dp)
-            .clickable {
-                context.startActivity(Intent(context, navigateTo))
-            },
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.primary,
-            contentColor = MaterialTheme.colorScheme.onPrimary
-        )
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = text.uppercase(),
-                style = MaterialTheme.typography.headlineMedium,
-                textAlign = TextAlign.Center,
-                fontSize = 27.sp
-
-            )
-        }
-    }
-}
---
+++ app/src/main/java/se/hkr/smarthouse/ui/composables/DevicesComposables.kt
@@ -0,0 +1,201 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package se.hkr.smarthouse.ui.composables
+
+import android.content.Intent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.House
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import se.hkr.smarthouse.data.Device
+import se.hkr.smarthouse.network.WSHelper
+
+
+class DevicesComposables() {
+    @Composable
+    fun TextInputCard(LCDText: MutableState<String>) {
+        ElevatedCard(
+            Modifier
+                .fillMaxWidth()
+                .padding(0.4.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+            )
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Column(
+                ) {
+                    OutlinedTextField(LCDText)
+                    LCDButton(message = LCDText)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun OutlinedTextField(text: MutableState<String>) {
+    val focusManager = LocalFocusManager.current
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth(),
+        value = text.value,
+        onValueChange = { text.value = it },
+        keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+        label = { Text("Your message here") }
+    )
+}
+
+@Composable
+fun LCDButton(message: MutableState<String>) {
+    Button(onClick = { WSHelper.sendMessage(message.value, "LCD", "message") }, modifier = Modifier
+        .padding(top = 16.dp)
+        .fillMaxWidth()) {
+        Text("Set new message")
+    }
+}
+
+@Composable
+fun DeviceSwitch(device: Device) {
+    Switch(
+        modifier = Modifier.semantics { contentDescription = "Demo" },
+        checked = device.status.value,
+        onCheckedChange = { isChecked ->
+            WSHelper.toggleDevice(device.status, device.endpoint)
+            device.status.value = isChecked
+        })
+}
+
+@Composable
+fun DeviceCard(device: Device) {
+    ElevatedCard(
+        Modifier
+            .fillMaxWidth()
+            .padding(0.4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+            contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+    ) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.Start,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                ) {
+                    Icon(imageVector = Icons.Rounded.House, contentDescription = null)
+                }
+
+                Column(
+                    horizontalAlignment = Alignment.Start,
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp, vertical = 8.dp)
+                ) {
+                    Text(
+                        text = device.displayName.uppercase(),
+                        style = MaterialTheme.typography.headlineMedium,
+                        modifier = Modifier.align(Alignment.Start),
+                    )
+                    Text(
+                        text = (if (device.status.value) device.statusMaskTrue else device.statusMaskFalse).uppercase(),
+                        style = MaterialTheme.typography.bodySmall,
+                        modifier = Modifier.align(Alignment.Start),
+                    )
+                }
+            }
+
+            Column(
+                horizontalAlignment = Alignment.End,
+                //modifier = Modifier
+                //.padding(16.dp),
+            ) {
+                DeviceSwitch(device)
+            }
+        }
+    }
+}
+
+@Composable
+fun SensorCard(
+    text: String,
+    navigateTo: Class<*>,
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+    ElevatedCard(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 0.dp, vertical = 8.dp)
+            .clickable {
+                context.startActivity(Intent(context, navigateTo))
+            },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = text.uppercase(),
+                style = MaterialTheme.typography.headlineMedium,
+                textAlign = TextAlign.Center,
+                fontSize = 27.sp
+
+            )
+        }
+    }
+}
